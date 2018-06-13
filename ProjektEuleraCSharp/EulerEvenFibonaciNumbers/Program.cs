8ec6a2d705103ed8ceb6330f6e287840bfc1caec
bugi353 <32601187+bugi353@users.noreply.github.com>
2018-06-13 19:58:43 +0200

    ProjektEuler

---
+++ ProjektEuleraCSharp/EulerEvenFibonaciNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EulerEvenFibonaciNumbers
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            long fibA =1, fibB =2;
+            long sum = fibB;
+
+            while (fibB < 4000000)
+            {   
+                if(fibB > fibA)
+                {
+                    fibA = fibA + fibB;
+                    if(fibA%2==0)
+                    {
+                        sum += fibA;
+                    }
+                }
+                else if(fibA>fibB)
+                {
+                    fibB = fibA + fibB;
+
+                    if (fibB % 2 == 0)
+                    {
+                        sum += fibB;
+                    }
+                }
+                
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProjektEuleraCSharp/MultiplesOf3And5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiplesOf3And5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            bool result;
+            
+            var result1 = Enumerable.Range(0,1000).Where(i => i% 3 == 0 || i% 5 == 0).Sum();  //
+            
+            for (int i=0; i<1000; i++)
+            {
+                result = i % 3==0 || i % 5 == 0;
+                if(result)
+                {
+                    suma += i;
+                }
+            }
+
+            System.Console.WriteLine(suma);
+            System.Console.ReadKey();
+        }
+    }
+}
