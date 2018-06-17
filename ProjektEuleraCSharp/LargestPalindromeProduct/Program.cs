d069c958e81f69216dcdec9b243155af676fee81
Piotr <bugi353@gmail.com>
2018-06-17 21:45:31 +0200

    ProjectEuler Exercise 3 and 4

---
+++ ProjektEuleraCSharp/LargestPalindromeProduct/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LargestPalindromeProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 0, d=0;
+            string liczba, palindrom ="";
+            
+            for(int i = 999; i>99; i--)
+            {
+                for(int j=999; j>99; j--)
+                {
+                    c = i * j;
+                    
+                    liczba = c.ToString();
+
+                    if(c<100000)
+                    {
+                        break;
+                    }
+
+                    for(int k=0; k<6; k++)
+                    {
+                        if(liczba[k] == liczba[5-k]&&k==5)
+                        {
+                            if(c>d)
+                            {
+                                palindrom = liczba;
+                                d = c;
+                            }
+                            
+
+                        }
+                        if (liczba[k] != liczba[5 - k])
+                        {
+                            break;
+                        }
+                    }
+                    
+                }
+            }
+            
+            Console.WriteLine(palindrom);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProjektEuleraCSharp/LargestPrimeFactor/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LargestPrimeFactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            long[] liczbyPierwsze = new long [1000];
+            double liczba = 600851475143;
+            long liczbaspr = 600851475143L;
+            //liczba = Console.Read();
+            long pierwiastek = (long)Math.Sqrt(liczba);
+            long a, b;
+
+            for (long i = 2; i <= pierwiastek; i++)
+            {
+
+
+                if (liczbaspr % i == 0)
+                {
+                    a = i;
+                    b = liczbaspr / a;
+                    for (double j = 2; j < a; j++)
+                    {
+                        if (a % j != 0 && j == a - 1)
+                        {
+                            liczbyPierwsze[x] = a;
+                            x++;
+                        }
+                        if (a % j == 0)
+                        {
+                            break;
+                        }
+                    }
+                    for (double k = 2; k < b; k++)
+                    {
+                        if (b % k != 0 && k == b - 1)
+                        {
+                            liczbyPierwsze[x] = b;
+                            x++;
+                        }
+                        if (b % k == 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+
+
+            }
+
+            for (int i = 0; i < 1000; i++)
+            {
+
+                if (liczbyPierwsze[i] != 0)
+                {
+                    Console.WriteLine(liczbyPierwsze[i]);
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
